2cd511796c572c2d7d92d4b928cee75d733a5805
Gedeonir <irafasha.jedy12@gmail.com>
2023-04-18 12:33:21 +0200

    Users home

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,6 +6,7 @@ import AppointmentForm from "../pages/AppointmentForm";
 import Doctors from "../pages/Doctors";
 
 
+import UsersHome from "../pages/UsersHome";
 
 const AppRoutes = (prop) => {
   
@@ -16,6 +17,8 @@ const AppRoutes = (prop) => {
           <Route path="/appointment-form" index element={<AppointmentForm/>} />
           <Route path="/Doctors" element={<Doctors/>} />
           <Route path="/Doctors/1/details"/>
+          <Route path="/appointment-form" element={<AppointmentForm/>} />
+          <Route path="dashboard" element={<UsersHome/>}></Route>
         </Routes>
       </div>
     );
